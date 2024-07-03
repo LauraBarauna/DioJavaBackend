4c3b0421ced545cb4be784bf2fbad23f75c21b80
LauraBarauna <lauravbarauna@gmail.com>
2024-07-03 19:02:36 -0300

    Finalizando STREAM API e entregando desafio

---
+++ src/src/edu/laura/bootcamp/modulo2/streamAPI/exemplos/ExemploSupplier.java
@@ -0,0 +1,20 @@
+package src.edu.laura.bootcamp.modulo2.streamAPI.exemplos;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class ExemploSupplier {
+    public static void main(String[] args) {
+        // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+        Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+
+        // Usar o Supplier para obter uma lista com 5 saudações
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+                .limit(5)
+                .toList();
+
+        // Imprimir as saudações geradas
+        listaSaudacoes.forEach(System.out::println);
+    }
+}
